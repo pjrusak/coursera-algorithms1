0b126b390d2ffebd44a87ef5e4b7072533e8b3b2
pjrusak <pjrusak@gmail.com>
2017-01-07 15:41:38 +0100

    Permutation client
    Changes to client code

--- Queues/Permutation.java
+++ Queues/Permutation.java
@@ -20,10 +20,7 @@ public class Permutation {
         int k = Integer.parseInt(args[0]);
         
         while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("")) {
-                q.enqueue(item);
-            }
+            q.enqueue(StdIn.readString());
         }
         for (int i = 0; i < k; i++) {
             StdOut.println(q.dequeue());
